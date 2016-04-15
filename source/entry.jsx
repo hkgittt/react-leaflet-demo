410c671539a812c934578c33d8d1d056c21aa464
Dev@Mesh::iMac <devdoomari@gmail.com>
2016-04-15 11:26:20 +0900

    [wip]

--- source/entry.jsx
+++ source/entry.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Map, Marker, Popup, TileLayer, GeoJson } from 'react-leaflet';
+import {
+  Map, CanvasTileLayer,
+  Marker, Popup,
+  TileLayer, MapLayer,
+  GeoJson ,
+} from 'react-leaflet';
+
+const Leaflet = require('expose?exports?L!leaflet');
+
 import 'leaflet/dist/leaflet.css';
+const googleMap = require('exports?L.Google!leaflet-plugins/layer/tile/Google');
 import './entry.scss';
+
+const asdasd = googleMap;
+debugger;
+
 
 class MapComp extends React.Component {
   constructor(props) {
@@ -17,14 +30,21 @@ class MapComp extends React.Component {
         'gangnam.json',
       ],
       geoOn: true,
+      counter: 0,
     };
   }
   render() {
     const markers = this.state.markers.map((position) => (
         <Marker position={position}>
           <Popup>
-            <div onClick={() => (this.setState({ geoOn: !this.state.geoOn }))}>
+            <div onClick={() => {
+              this.setState({
+                geoOn: !this.state.geoOn,
+                counter: this.state.counter + 1,
+              });
+            }}>
               CLICK ME
+              {this.state.counter}
             </div>
           </Popup>
         </Marker>
@@ -41,10 +61,11 @@ class MapComp extends React.Component {
     }
     return (
       <Map center={this.state.markers[0]} zoom={13}>
-        <TileLayer
+        {/*<TileLayer
           url="http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-        />
+        />*/}
+        <CanvasTileLayer map={googleMap}/>
         {markers}
         {geojsons}
       </Map>
